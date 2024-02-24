8f1197d48db79a24c08c1385ef04adf706740aa5
Mateo <mateodacunha42@gmail.com>
2024-02-24 15:14:50 +0100

    ajout de test et du compte rendu

--- src/command.c
+++ src/command.c
@@ -6,10 +6,12 @@
 
 
 void command_quit(){
+    //On supprime Le dossier qui contient les pipes
+    //et comme on exec une commande le reste ne sera pas executé
     execl("/bin/rm","rm","-r","Pipe",NULL);
 }
 
-void Interrup(int sig){
+void Interrup(int sig){//gestion SIGINT pour les fils
 	exit(0);
 }
 
@@ -19,39 +21,46 @@ int command_redirection(char* out,char* in,char** cmd,int nombre_cmd,int flag_de
     int fd_pipeout,fd_pipein;
     char pipe_name[TAILLE_MAX];
     char pipe_num[TAILLE_MAX];
+
+    //on recupere le nom du pipe
     strcpy(pipe_name,"Pipe/pipe");
 	sprintf(pipe_num,"%d",nombre_cmd);
 	strcat(pipe_name,pipe_num);
+
+    //on regarde si ya un quit
     for (i=0; cmd[i]!=0; i++) {
 		if(!(strcmp(cmd[i],"quit"))){
 		printf("\n");
 		command_quit();
 	    }
     }
-    pid=Fork();
+
+    pid=Fork();// on cree un fils
     if(pid==-1){
         printf("error : Fork Issue \n");
         exit(1);
     }
-    if(pid==0){ // Daughter
-        Signal(SIGINT, Interrup);
-        if((!flag_dernier) && (nombre_cmd==0)){
+    
+    if(pid==0){ // Child
+        Signal(SIGINT, Interrup);//Changement de SIGINT pour les childs
+        
+        if((!flag_dernier) && (nombre_cmd==0)){//si c'est la derniere commande du pipe
             fd_pipeout=open(pipe_name,O_WRONLY, 0644);
             dup2(fd_pipeout,1);
         }
-        if(nombre_cmd>0){
+        if(nombre_cmd>0){ //si ce n'est pas la premiere commande 
             if(!flag_dernier){
                 fd_pipeout=open(pipe_name,O_WRONLY, 0644);
                 dup2(fd_pipeout,1);
             }
-            strcpy(pipe_name,"Pipe/pipe");
+            strcpy(pipe_name,"/tmp/pipe");
             sprintf(pipe_num,"%d",nombre_cmd-1);
             strcat(pipe_name,pipe_num);
             fd_pipein=open(pipe_name,O_RDONLY, 0644);
             dup2(fd_pipein,0);
             
         }
-        if(out){
+        if(out){//si il y a une redirection de la sortie
             fd_out=open(out, O_CREAT | O_WRONLY | O_TRUNC , 0644);
             if(fd_out < 0){
                 printf("%s : Permission denied \n",out);
@@ -59,7 +68,7 @@ int command_redirection(char* out,char* in,char** cmd,int nombre_cmd,int flag_de
             }
             dup2(fd_out,1);
         }
-        if(in){
+        if(in){//si il y a une redirection de l'entrée
             fd_in=open(in,O_RDONLY, 0644);
             if(fd_in < 0){
                 printf("%s : Permission denied \n",in);
@@ -67,11 +76,13 @@ int command_redirection(char* out,char* in,char** cmd,int nombre_cmd,int flag_de
             }
             dup2(fd_in,0);
         }
-        err = execvp(cmd[0],cmd);
-        if(err<0){
+        err = execvp(cmd[0],cmd); //on execute la commande 
+        if(err<0){//si la commande c'est mal passé
             printf("%s : command not found \n",cmd[0]);
             exit(1);
         }
+
+        //fermeture des pipes et fichiers ouvert precedemment
         if(!flag_dernier){
             close(fd_pipeout);
         }
@@ -87,7 +98,7 @@ int command_redirection(char* out,char* in,char** cmd,int nombre_cmd,int flag_de
         exit(0);
     }
 
-    else{ //Mommy
+    else{ //Father
         if(flag_background){
             errset=setpgid(pid,0);
             if(errset<0){
--- src/shell.c
+++ src/shell.c
@@ -9,11 +9,11 @@
 #include "csapp.h"
 #include "command.h"
 
-void terminaison(int sig){
-	waitpid(-1, NULL, WNOHANG|WUNTRACED);
+void terminaison(int sig){ //gestion de SIGCHLD
+	waitpid(-1, NULL, WNOHANG|WUNTRACED); 
 }
 
-void ignorer(int sig){
+void ignorer(int sig){ //gestion de SIGINT 
 	printf("\n");
 }
 
@@ -22,14 +22,21 @@ int main()
 	signal(SIGINT, ignorer);
 	Signal(SIGCHLD, terminaison);
 	while (1) {
+
 		struct cmdline *l;
-		int i, j,perr,flag_dernier,pid;
+		int i,perr,flag_dernier;
 		char pipe_name[TAILLE_MAX];
 		char pipe_num[TAILLE_MAX];
+
+		//affichage de la ligne principal du shell
 		char* pwd=getenv("PWD");
 		char* user=getenv("USER");
-		printf("%s:%s>",user,pwd);
+		printf("\033[31m%s\033[97m:\033[96m%s>\033[97m",user,pwd);
+
+		//on recupere l'entrée
 		l = readcmd(); 
+
+		//On crée un dossier dans lequel on mettra les pipes
 		mkdir("Pipe",0755);
 		/* If input stream closed, normal termination */
 		if (!l) {
@@ -45,16 +52,19 @@ int main()
 		
 		/* Display each command of the pipe */
 		for (i=0; l->seq[i]!=0; i++) {
+			//on crée un Pipe 
 			strcpy(pipe_name,"Pipe/pipe");
 			sprintf(pipe_num,"%d",i);
 			strcat(pipe_name,pipe_num);
+
+			// si il y a plusieurs commande qui s'enchainent
 			if(l->seq[i+1]!=0){
 				perr=mkfifo(pipe_name,0644);
 				if(perr<0 && errno!=EEXIST){
 					printf("error : Pipe Issue\n");
 					exit(0);
 				}
-				flag_dernier=0;
+				flag_dernier = 0;//on definit si ce sera le dernier pipe a creer
 			}
 			else{
 				flag_dernier = 1;
@@ -65,10 +75,10 @@ int main()
 			command_redirection(l->out,l->in,cmd,i,flag_dernier,l->back);
 			//printf("\n");
 		}
-		
+		//on attend tous les processus sauf ceux qui ont été lancé en arriere-plan
 		if(!(l->back)){
 			while(waitpid(0,NULL,0)>0){};
-		}
+		} 
 		
 	}
 }
